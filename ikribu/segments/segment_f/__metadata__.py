dbc1167bda436e1d704f0535fa10b4296a26220c
Trevor Bača <trevor.baca@gmail.com>
2016-02-11 14:56:55 -0600

    [F].

--- ikribu/segments/segment_f/__metadata__.py
+++ ikribu/segments/segment_f/__metadata__.py
@@ -4,7 +4,35 @@ from abjad import *
 
 metadata = datastructuretools.TypedOrderedDict(
     [
+        (
+            'end_clefs_by_staff',
+            datastructuretools.TypedOrderedDict(
+                [
+                    ('Bass Clarinet Music Staff', 'treble'),
+                    ('Cello Music Staff', 'bass'),
+                    ('Cello RH Music Staff', 'percussion'),
+                    ('Viola Music Staff', 'alto'),
+                    ('Viola RH Music Staff', None),
+                    ('Violin Music Staff', 'treble'),
+                    ('Violin RH Music Staff', None),
+                    ]
+                ),
+            ),
+        (
+            'end_instruments_by_context',
+            datastructuretools.TypedOrderedDict(
+                [
+                    ('Bass Clarinet Music Staff', 'bass clarinet'),
+                    ('Cello Staff Group', 'cello'),
+                    ('Viola Staff Group', 'viola'),
+                    ('Violin Staff Group', 'violin'),
+                    ]
+                ),
+            ),
+        ('end_tempo', 'night'),
+        ('end_time_signature', '7/4'),
         ('first_bar_number', 78),
+        ('measure_count', 8),
         ('name', 'F'),
         ('segment_count', 18),
         ('segment_number', 7),
