5d06b6e5445fcc83896521fd033e9bda9ae33164
Trevor Bača <trevor.baca@gmail.com>
2016-02-11 17:08:35 -0600

    [J].

--- ikribu/segments/segment_k/__metadata__.py
+++ ikribu/segments/segment_k/__metadata__.py
@@ -4,6 +4,7 @@ from abjad import *
 
 metadata = datastructuretools.TypedOrderedDict(
     [
+        ('first_bar_number', 126),
         ('name', 'K'),
         ('segment_count', 18),
         ('segment_number', 12),
