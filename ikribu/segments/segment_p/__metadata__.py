e38106bacbc70f0bc0ca4f4fbbf04ab4d9d7e2c3
Trevor Bača <trevor.baca@gmail.com>
2016-02-15 17:42:53 -0600

    [P].

--- ikribu/segments/segment_p/__metadata__.py
+++ ikribu/segments/segment_p/__metadata__.py
@@ -9,9 +9,9 @@ metadata = datastructuretools.TypedOrderedDict(
             datastructuretools.TypedOrderedDict(
                 [
                     ('Bass Clarinet Music Staff', 'treble'),
-                    ('Cello Music Staff', 'bass'),
+                    ('Cello Music Staff', 'tenor'),
                     ('Cello RH Music Staff', 'percussion'),
-                    ('Viola Music Staff', 'alto'),
+                    ('Viola Music Staff', 'treble'),
                     ('Viola RH Music Staff', 'percussion'),
                     ('Violin Music Staff', 'treble'),
                     ('Violin RH Music Staff', 'percussion'),
@@ -32,7 +32,7 @@ metadata = datastructuretools.TypedOrderedDict(
         ('end_tempo', 'night'),
         ('end_time_signature', '1/4'),
         ('first_bar_number', 239),
-        ('measure_count', 9),
+        ('measure_count', 8),
         ('name', 'P'),
         ('segment_count', 18),
         ('segment_number', 17),
