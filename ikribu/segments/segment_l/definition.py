8837c4df00a4436303500884da01c3d337271d94
Trevor Bača <trevor.baca@gmail.com>
2016-02-15 13:31:51 -0600

    [L].

--- ikribu/segments/segment_l/definition.py
+++ ikribu/segments/segment_l/definition.py
@@ -39,8 +39,8 @@ spacing_specifier = baca.tools.SpacingSpecifier(
     )
 
 segment_maker = baca.tools.SegmentMaker(
-    label_clock_time=True,
-    label_stage_numbers=True,
+    #label_clock_time=True,
+    #label_stage_numbers=True,
     measures_per_stage=measures_per_stage,
     score_package=ikribu,
     spacing_specifier=spacing_specifier,
@@ -83,13 +83,25 @@ segment_maker.append_specifiers(
     )
 
 segment_maker.append_specifiers(
-    (vc, stages(1, 6)),
-    baca.rhythm.make_repeated_duration_rhythm_specifier((1, 4)),
+    (vc, [stages(1), stages(2), stages(3), stages(4), stages(5), stages(6)]),
+    baca.rhythm.make_tied_repeated_duration_rhythm_specifier((1, 4)),
     )
 
 ###############################################################################
 #################################### COLOR ####################################
 ###############################################################################
+
+segment_maker.append_specifiers(
+    (bcl, stages(1, 7)),
+    [
+        baca.markup.make_markup(
+            'breathe discreetly before any downbeat as needed'
+            ' (but do not circular breathe)'
+            ),
+        baca.pitch.pitches('C2'),
+        Dynamic('ppp'),
+        ],
+    )
 
 segment_maker.append_specifiers(
     ([vn, va], stages(1, 8)),
@@ -101,11 +113,114 @@ segment_maker.append_specifiers(
     )
 
 segment_maker.append_specifiers(
+    (vn, stages(6, 8)),
+    [
+        baca.dynamics.make_hairpins(
+            [
+                'mp < mf', 'mf > mp', 'mp < f', 'f > mf',
+                'mf < f', 'f > mf', 'mf < ff', 'ff > f',
+                'f < ff', 'ff > f', 'f < fff', 'fff > ff',
+                ],
+            enchain_hairpins=True,
+            include_following_rests=True,
+            span=[4],
+            ),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (va, stages(4, 8)),
+    [
+        baca.dynamics.make_hairpins(
+            [
+                'p > pp', 'pp < p', 'p > pp', 'pp < mp',
+                'mp > p', 'p < mp', 'mp > p', 'p < mf',
+                'mf > mp', 'mp < mf', 'mf > mp', 'mp < f',
+                'f > mf', 'mf < f', 'f > mf', 'mf < ff',
+                'ff > f', 'f < ff', 'ff > f', 'f < fff',
+                ],
+            enchain_hairpins=True,
+            include_following_rests=True,
+            span=[4],
+            ),
+        ],
+    )
+
+segment_maker.append_specifiers(
     (vc, stages(1, 8)),
     [
         baca.pitch.pitches('C4'),
         baca.spanners.one_line_staff(),
         baca.spanners.percussion_staff(),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vc, stages(1, 6)),
+    [
+        baca.dynamics.make_hairpins(
+            ['p < f', 'f > p'],
+            enchain_hairpins=True,
+            include_following_rests=True,
+            span=[
+                9, 8, 4, 5,
+                5, 3, 8, 4,
+                5, 4, 5, 4,
+                3, 8, 8, 9,
+                8, 4, 3, 9,
+                8, 5, 5, 4,
+                ],
+            ),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vc, stages(2)),
+    [
+        baca.markup.make_boxed_markup_lines([
+            'graincircle:',
+            'π/3 every quarter note'
+            ]),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vc, stages(3)),
+    [
+        baca.markup.make_boxed_markup_lines([
+            'graincircle:',
+            'π/4 every quarter note'
+            ]),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vc, stages(4)),
+    [
+        baca.markup.make_boxed_markup_lines([
+            'graincircle:',
+            'π/3 every quarter note'
+            ]),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vc, stages(5)),
+    [
+        baca.markup.make_boxed_markup_lines([
+            'graincircle:',
+            'π/2 every quarter note'
+            ]),
+        ],
+    )
+
+segment_maker.append_specifiers(
+    (vc, stages(6)),
+    [
+        baca.markup.make_boxed_markup_lines([
+            'graincircle:',
+            'π/3 every quarter note'
+            ]),
         ],
     )
 
