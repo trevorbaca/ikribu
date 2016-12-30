77ac44c92309a1de154de4e29d66fa6000d965f2
Trevor Bača <trevor.baca@gmail.com>
2016-12-30 11:15:11 -0600

    Ported transform calls.

--- ikribu/tools/make_clb_pitch_specifier.py
+++ ikribu/tools/make_clb_pitch_specifier.py
@@ -5,7 +5,7 @@ from abjad.tools import sequencetools
 
 def make_clb_pitch_specifier(rotation=None):
     pitches = [[-1, 0, 2, 2, 0], [0, 2, -1, 0], [-1, 2, 0, 2]]
-    pitches = baca.tools.helianthate(pitches, -1, -1)
+    pitches = baca.transforms.helianthate(pitches, -1, -1)
     pitches = sequencetools.Sequence(pitches)
     pitches = pitches.rotate(n=rotation)
     pitches = pitches.flatten()
--- ikribu/tools/make_inscription_rhythm_specifier.py
+++ ikribu/tools/make_inscription_rhythm_specifier.py
@@ -6,7 +6,7 @@ from abjad.tools import sequencetools
 
 def make_inscription_rhythm_specifier():
     counts = [[2, 2, 1, -1, 3], [-18], [1, 1], [1, -2, 2, 3], [-10]]
-    counts = baca.tools.helianthate(counts, -1, -1)
+    counts = baca.transforms.helianthate(counts, -1, -1)
     counts = sequencetools.Sequence(counts).flatten()
     extra_counts_per_division = [2, 4, 0]
     rhythm_maker = rhythmmakertools.TaleaRhythmMaker(
