1c221eec75909d4f1feda8a9473c64c87d4e281c
Trevor Bača <trevor.baca@gmail.com>
2017-01-29 10:26:14 -0600

    Updated flatten transforms.

--- ikribu/materials/time_signatures/definition.py
+++ ikribu/materials/time_signatures/definition.py
@@ -7,7 +7,7 @@ numerators = [[7, 3, 2], [8, 7], [4, 4, 3]]
 numerator_groups = baca.transforms.helianthate(numerators, -1, 1)
 assert len(numerator_groups) == 18, repr(len(numerator_groups))
 lengths = [len(_) for _ in numerator_groups]
-numerators = abjad.sequencetools.flatten_sequence(numerator_groups)
+numerators = baca.Sequence(numerator_groups).flatten()
 time_signatures = [abjad.TimeSignature((_, 4)) for _ in numerators]
 time_signature_groups = abjad.sequencetools.partition_sequence_by_counts(
     time_signatures,
