f3385251085b5a3f88f9166ea2c58abb7b78a255
Trevor Bača <trevor.baca@gmail.com>
2017-01-29 19:44:02 -0600

    Updated partitions.

--- ikribu/materials/time_signatures/definition.py
+++ ikribu/materials/time_signatures/definition.py
@@ -9,8 +9,7 @@ assert len(numerator_groups) == 18, repr(len(numerator_groups))
 lengths = [len(_) for _ in numerator_groups]
 numerators = baca.Sequence(numerator_groups).flatten()
 time_signatures = [abjad.TimeSignature((_, 4)) for _ in numerators]
-time_signature_groups = abjad.sequencetools.partition_sequence_by_counts(
-    time_signatures,
+time_signature_groups = baca.Sequence(time_signatures).partition_by_counts(
     lengths,
     )
 time_signatures = time_signature_groups
