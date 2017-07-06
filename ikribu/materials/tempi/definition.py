152f02d8a97feaee7e2826e82ab28cfbcf1ba45d
Trevor Bača <trevor.baca@gmail.com>
2017-07-06 09:29:07 -0500

    Changed Tempo to MetronomeMark.

--- ikribu/materials/tempi/definition.py
+++ ikribu/materials/tempi/definition.py
@@ -5,18 +5,18 @@ import abjad
 tempi = abjad.TypedOrderedDict([
     (
         'incisions',
-        abjad.Tempo(abjad.Duration(1, 4), 58),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 58),
         ),
     (
         'inscription',
-        abjad.Tempo(abjad.Duration(1, 4), 66),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 66),
         ),
     (
         'night',
-        abjad.Tempo(abjad.Duration(1, 4), 42),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 42),
         ),
     (
         'windows',
-        abjad.Tempo(abjad.Duration(1, 4), 104),
+        abjad.MetronomeMark(abjad.Duration(1, 4), 104),
         ),
     ])
