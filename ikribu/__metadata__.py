dec708bd5b201f297de5965ab0b5952b2cfd4c61
Trevor Bača <trevor.baca@gmail.com>
2017-08-16 11:32:00 -0500

    Cleaned up metadat.

--- ikribu/__metadata__.py
+++ ikribu/__metadata__.py
@@ -4,10 +4,8 @@ import abjad
 metadata = abjad.TypedOrderedDict(
     [
         ('catalog_number', 'AWN-014'),
-        ('composer', 'Bača'),
+        ('composer', 'Trevor Bača'),
         ('forces_tagline', 'for bass clarinet, violin, viola \& cello'),
-        ('paper_size', '17 x 11 in'),
-        ('price', '\\$60 / \\euro 54'),
         ('title', 'Ikribu'),
         ('year', 2016),
         ]
