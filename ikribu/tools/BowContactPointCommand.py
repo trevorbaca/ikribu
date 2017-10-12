533a5ee41932c6de1fdae24304b127d03426ea65
Trevor Bača <trevor.baca@gmail.com>
2017-10-12 10:46:09 -0500

    Cleaned up bow contact point command.

--- ikribu/tools/BowContactPointCommand.py
+++ ikribu/tools/BowContactPointCommand.py
@@ -3,7 +3,7 @@ import baca
 
 
 class BowContactPointCommand(baca.Command):
-    r'''Bow contact point specifier.
+    r'''Bow contact point command.
     '''
 
     ### CLASS VARIABLES ###
@@ -23,28 +23,38 @@ class BowContactPointCommand(baca.Command):
 
     ### INITIALIZER ###
 
-    def __init__(self, rotation=None):
+    def __init__(self, rotation=None, selector=None):
+        baca.Command.__init__(self, selector=selector)
         self._rotation = rotation
 
     ### SPECIAL METHODS ###
 
-    def __call__(self, logical_ties=None):
+    def __call__(self, selections=None):
+        r'''Calls command on leaf `selections`.
+
+        Returns none.
+        '''
+        if selections is None:
+            return
+        if self.selector is not None:
+            selections = self.selector(selections)
         bow_contact_points = baca.sequence(self._bow_contact_points)
         bow_contact_points = bow_contact_points.rotate(n=self.rotation)
         bow_contact_points = bow_contact_points.flatten(depth=1)
         bow_contact_points = abjad.CyclicTuple(bow_contact_points)
-        for i, logical_tie in enumerate(logical_ties):
+        plts = abjad.iterate(selections).by_logical_tie(pitched=True)
+        for i, plt in enumerate(plts):
             bow_contact_point = bow_contact_points[i]
             numerator, denominator = bow_contact_point
             markup = abjad.Markup.fraction(numerator, denominator)
             markup = abjad.new(markup, direction=Up)
-            abjad.attach(markup, logical_tie.head)
+            abjad.attach(markup, plt.head)
 
     ### PUBLIC PROPERTIES ###
 
     @property
     def rotation(self):
-        r'''Gets rotation of specifier.
+        r'''Gets rotation.
 
         Returns integer or none.
         '''
