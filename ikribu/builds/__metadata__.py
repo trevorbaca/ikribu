356310cbaa9101628062624ce1ad5d87081d61b6
Trevor Bača <trevor.baca@gmail.com>
2017-11-26 15:35:12 -0600

    Integrating layout changes.

---
+++ ikribu/builds/__metadata__.py
@@ -0,0 +1,38 @@
+import abjad
+
+
+metadata = abjad.TypedOrderedDict(
+    [
+        (
+            'ledger',
+            abjad.TypedOrderedDict(
+                [
+                    (
+                        'break_measures',
+                        [
+                            16,
+                            33,
+                            49,
+                            63,
+                            75,
+                            87,
+                            99,
+                            108,
+                            125,
+                            143,
+                            156,
+                            166,
+                            181,
+                            194,
+                            208,
+                            218,
+                            229,
+                            239,
+                            249,
+                            ],
+                        ),
+                    ]
+                ),
+            ),
+        ]
+    )
