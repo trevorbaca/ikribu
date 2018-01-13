8a741c56179467f8fcd7a86862212c28bb635c65
Trevor Bača <trevor.baca@gmail.com>
2018-01-12 18:27:41 -0600

    ARCH-A build.

--- ikribu/builds/__metadata__.py
+++ ikribu/builds/__metadata__.py
@@ -4,6 +4,40 @@ import abjad
 metadata = abjad.TypedOrderedDict(
     [
         (
+            'arch-a-score',
+            abjad.TypedOrderedDict(
+                [
+                    (
+                        'break_measures',
+                        [
+                            1,
+                            8,
+                            17,
+                            30,
+                            40,
+                            51,
+                            60,
+                            75,
+                            85,
+                            97,
+                            108,
+                            125,
+                            143,
+                            155,
+                            165,
+                            178,
+                            194,
+                            208,
+                            222,
+                            235,
+                            247,
+                            252,
+                            ],
+                        ),
+                    ]
+                ),
+            ),
+        (
             'ledger-score',
             abjad.TypedOrderedDict(
                 [
