f9b08cc4c6580df15628dab3635853464afc426d
Trevor Bača <trevor.baca@gmail.com>
2018-01-14 10:30:46 -0600

    Cleaned up build metadata.

--- ikribu/builds/ledger-score/__metadata__.py
+++ ikribu/builds/ledger-score/__metadata__.py
@@ -3,6 +3,31 @@ import abjad
 
 metadata = abjad.TypedOrderedDict(
     [
+        (
+            'break_measures',
+            [
+                1,
+                16,
+                33,
+                49,
+                63,
+                75,
+                85,
+                97,
+                108,
+                125,
+                143,
+                155,
+                165,
+                178,
+                194,
+                208,
+                222,
+                235,
+                247,
+                252,
+                ],
+            ),
         ('paper_size', 'ledger'),
         ('price', '\\$60 / \\euro 60'),
         ]
