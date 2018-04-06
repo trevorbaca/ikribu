e944e32429e569c01646a2d209af0702695eb736
Trevor Bača <trevor.baca@gmail.com>
2018-04-06 11:55:51 -0500

    Cleaned up segment spacing.

--- ikribu/segments/C/layout.py
+++ ikribu/segments/C/layout.py
@@ -11,6 +11,5 @@ spacing = baca.scorewide_spacing(
     __file__,
     breaks=breaks,
     fallback_duration=(1, 12),
-    fermata_measure_duration=(1, 4),
     )
 spacing.override([39, 43, 47, 51], (1, 24))
--- ikribu/segments/M/layout.py
+++ ikribu/segments/M/layout.py
@@ -15,6 +15,5 @@ spacing = baca.scorewide_spacing(
     __file__,
     breaks=breaks,
     fallback_duration=(1, 12),
-    fermata_measure_duration=(1, 4),
     )
 spacing.override([182, 185], (1, 16))
