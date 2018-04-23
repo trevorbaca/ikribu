687f7cf0a238e9c49ececdc46637ccadabc65a57
Trevor Bača <trevor.baca@gmail.com>
2018-04-23 12:10:03 -0500

    Cleaned up bow contact points.

--- ikribu/tools/bcps.py
+++ ikribu/tools/bcps.py
@@ -5,12 +5,12 @@ import baca
 def bcps(rotation=None):
     r'''Makes bow contact points.
     '''
-    return baca.bcps(
-        bcps=(
-            [(0, 7), (4, 7), (5, 7), (6, 7), (7, 7), (6, 7)],
-            [(7, 7), (0, 7), (7, 7), (0, 7), (7, 7)],
-            [(0, 7), (4, 7), (5, 7), (6, 7), (7, 7), (6, 7), (7, 7)],
-            [(0, 4), (1, 4), (2, 4), (1, 4)],
-            ),
-        rotation=rotation,
-        )
+    bcps = baca.sequence([
+        [(0, 7), (4, 7), (5, 7), (6, 7), (7, 7), (6, 7)],
+        [(7, 7), (0, 7), (7, 7), (0, 7), (7, 7)],
+        [(0, 7), (4, 7), (5, 7), (6, 7), (7, 7), (6, 7), (7, 7)],
+        [(0, 4), (1, 4), (2, 4), (1, 4)],
+        ])
+    bcps = bcps.rotate(n=rotation)
+    bcps = bcps.flatten(depth=1)
+    return baca.bcps(bcps=bcps)
