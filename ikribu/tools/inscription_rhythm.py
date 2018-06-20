110376466c56cf59093fb9dc5fad0cf8c94e4227
Trevor Bača <trevor.baca@gmail.com>
2018-06-20 14:25:27 -0500

    Ported helianthation.

--- ikribu/tools/inscription_rhythm.py
+++ ikribu/tools/inscription_rhythm.py
@@ -4,9 +4,11 @@ from abjadext import rmakers
 
 
 def inscription_rhythm():
-    counts = [[2, 2, 1, -1, 3], [-18], [1, 1], [1, -2, 2, 3], [-10]]
-    counts = baca.helianthate(counts, -1, -1)
-    counts = abjad.sequence(counts).flatten()
+    counts = baca.sequence([
+        [2, 2, 1, -1, 3], [-18], [1, 1], [1, -2, 2, 3], [-10],
+        ])
+    counts = counts.helianthate(-1, -1)
+    counts = counts.flatten()
     extra_counts_per_division = [2, 4, 0]
     rhythm_maker = rmakers.TaleaRhythmMaker(
         extra_counts_per_division=extra_counts_per_division,
