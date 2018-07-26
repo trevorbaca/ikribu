1bc0ef3a91610afdcd3c85137d8375069e843e80
Trevor Bača <trevor.baca@gmail.com>
2018-07-26 09:57:53 -0500

    Cleaned up mask keywords.

--- ikribu/tools/bow_rhythm.py
+++ ikribu/tools/bow_rhythm.py
@@ -4,9 +4,14 @@ from abjadext import rmakers
 
 
 def bow_rhythm(
-    logical_tie_masks=rmakers.silence([4, 19], 20),
+    *,
+    ltmask=rmakers.silence([4, 19], 20),
     rotation=None,
     ):
+    if ltmask is None:
+        logical_tie_masks = None
+    else:
+        logical_tie_masks = [ltmask]
     extra_counts_per_division = abjad.sequence([-1, 0, 1, 2])
     extra_counts_per_division = extra_counts_per_division.rotate(
         n=rotation
