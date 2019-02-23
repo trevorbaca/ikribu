c4aa230bee3bcce9fd7e4d1624e95b8606ca5d5c
Trevor Bača <trevor.baca@gmail.com>
2019-02-23 11:28:04 -0600

    Cleaned up metadata.

--- ikribu/__metadata__.py
+++ ikribu/__metadata__.py
@@ -3,7 +3,7 @@ import abjad
 
 metadata = abjad.OrderedDict(
     [
-        ('catalog_number', 'AWN-014'),
+        ('catalog_number', 'AWN-018'),
         ('composer', 'Trevor Bača'),
         (
             'fermata_measure_numbers',
