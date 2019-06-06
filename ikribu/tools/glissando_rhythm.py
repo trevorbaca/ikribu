f556f3b8d42be1f4c60a363dc501611ecd3c88f9
Trevor Bača <trevor.baca@gmail.com>
2019-06-05 23:26:15 -0500

    Blacked code

--- ikribu/tools/glissando_rhythm.py
+++ ikribu/tools/glissando_rhythm.py
@@ -15,9 +15,7 @@ def glissando_rhythm(
     extra_counts = extra_counts.rotate(n=rotation_2)
     return baca.rhythm(
         rhythm_maker=rmakers.TaleaRhythmMaker(
-            beam_specifier=rmakers.BeamSpecifier(
-                beam_each_division=True,
-            ),
+            beam_specifier=rmakers.BeamSpecifier(beam_each_division=True),
             extra_counts_per_division=extra_counts,
             talea=rmakers.Talea(counts=counts, denominator=16),
             tie_specifier=rmakers.TieSpecifier(strip_ties=True),
--- ikribu/tools/vigil_rhythm.py
+++ ikribu/tools/vigil_rhythm.py
@@ -9,9 +9,7 @@ def vigil_rhythm() -> baca.RhythmCommand:
     """
     return baca.rhythm(
         rhythm_maker=rmakers.TaleaRhythmMaker(
-            beam_specifier=rmakers.BeamSpecifier(
-                beam_each_division=True,
-            ),
+            beam_specifier=rmakers.BeamSpecifier(beam_each_division=True),
             talea=rmakers.Talea(counts=[16, -1], denominator=4),
             tie_specifier=rmakers.TieSpecifier(repeat_ties=True),
         ),
