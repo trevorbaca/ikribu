7b2d801a04dbec4e52fca434d51062356a1a2050
Trevor Bača <trevor.baca@gmail.com>
2019-06-12 16:26:07 -0400

    Cleaned up tuplet specifiers.

--- ikribu/tools/bcl_color_rhythm.py
+++ ikribu/tools/bcl_color_rhythm.py
@@ -15,13 +15,11 @@ def bcl_color_rhythm(
     extra_counts_per_division = extra_counts_per_division.rotate(n=rotation_2)
     return baca.rhythm(
         rhythm_maker=rmakers.TaleaRhythmMaker(
+            rmakers.TupletSpecifier(extract_trivial=True, trivialize=True),
             beam_specifier=rmakers.BeamSpecifier(beam_each_division=True),
             extra_counts_per_division=extra_counts_per_division,
             talea=rmakers.Talea(counts=counts, denominator=8),
             tie_specifier=rmakers.TieSpecifier(repeat_ties=True),
-            tuplet_specifier=rmakers.TupletSpecifier(
-                extract_trivial=True, trivialize=True
-            ),
         ),
         tag="ikribu.bcl_color_rhythm",
     )
