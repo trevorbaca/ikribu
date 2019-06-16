1db317d22acb4aa71eff09b7d0f8ae03f2bf4521
Trevor Bača <trevor.baca@gmail.com>
2019-06-16 16:28:33 -0500

    Cleaned up tie specifiers.

--- ikribu/tools/vigil_rhythm.py
+++ ikribu/tools/vigil_rhythm.py
@@ -10,9 +10,9 @@ def vigil_rhythm() -> baca.RhythmCommand:
     return baca.rhythm(
         rhythm_maker=rmakers.TaleaRhythmMaker(
             rmakers.TupletSpecifier(extract_trivial=True),
+            rmakers.TieSpecifier(repeat_ties=True),
             rmakers.BeamSpecifier(beam_each_division=True),
             talea=rmakers.Talea(counts=[16, -1], denominator=4),
-            tie_specifier=rmakers.TieSpecifier(repeat_ties=True),
         ),
         tag="ikribu.vigil_rhythm",
     )
