180167cd7ab447f8bf258fe40670c2605f7ba0e0
Trevor Bača <trevor.baca@gmail.com>
2019-06-17 13:33:31 -0500

    Cleaned up tuplet specifiers.

--- ikribu/tools/clb_rhythm.py
+++ ikribu/tools/clb_rhythm.py
@@ -9,12 +9,12 @@ def clb_rhythm(*, extra_counts: abjad.IntegerSequence) -> baca.RhythmCommand:
     """
     return baca.rhythm(
         rhythm_maker=rmakers.EvenDivisionRhythmMaker(
+            rmakers.TupletSpecifier(
+                diminution=True, extract_trivial=True, force_fraction=True
+            ),
             rmakers.BeamSpecifier(beam_each_division=True),
             denominators=[8],
             extra_counts_per_division=extra_counts,
-            tuplet_specifier=rmakers.TupletSpecifier(
-                diminution=True, extract_trivial=True, force_fraction=True
-            ),
         ),
         tag="ikribu.clb_rhythm",
     )
