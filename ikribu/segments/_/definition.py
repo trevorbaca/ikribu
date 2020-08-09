ce5468c624a3f77ef21a5072f14814f1cbb1e9e1
Trevor Bača <trevor.baca@gmail.com>
2020-08-09 13:08:34 -0400

    Cleaned up path management.

--- ikribu/segments/_/definition.py
+++ ikribu/segments/_/definition.py
@@ -22,7 +22,7 @@ maker = baca.SegmentMaker(
     ],
     check_all_are_pitched=True,
     fermata_measure_empty_overrides=fermata_measures,
-    segment_directory=abjad.Path(__file__).resolve().parent,
+    segment_directory=baca.Path(__file__).resolve().parent,
     time_signatures=time_signatures,
     validate_measure_count=2,
 )
