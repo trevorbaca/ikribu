ca628c8dc040f6ff3853405bacfc478feea6b988
Trevor Bača <trevor.baca@gmail.com>
2021-04-28 09:52:43 -0400

    Cleaned up selectors.

--- ikribu/segments/18/definition.py
+++ ikribu/segments/18/definition.py
@@ -137,6 +137,6 @@ maker(
         baca.rehearsal_mark_down(),
         baca.rehearsal_mark_padding(12),
         baca.rehearsal_mark_self_alignment_x(abjad.Right),
-        selector=baca.leaves().rleak()[-1],
+        selector=baca.selectors.rleaf(-1),
     ),
 )
