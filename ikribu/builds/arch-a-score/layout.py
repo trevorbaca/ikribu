4040c7f071a6e9f300fbe3a1957fa70f7430850d
Trevor Bača <trevor.baca@gmail.com>
2021-08-23 12:16:54 -0400

    Cleaned up layout files.

--- ikribu/builds/arch-a-score/layout.py
+++ ikribu/builds/arch-a-score/layout.py
@@ -3,75 +3,76 @@ import baca
 spacing = baca.SpacingSpecifier(
     breaks=baca.breaks(
         baca.page(
+            1,
             baca.system(measure=1, y_offset=80, distances=(15, 20)),
             baca.system(measure=7, y_offset=190, distances=(15, 20)),
-            number=1,
         ),
         baca.page(
+            2,
             baca.system(measure=16, y_offset=30, distances=(15, 20)),
             baca.system(measure=26, y_offset=150, distances=(15, 20)),
-            number=2,
         ),
         baca.page(
+            3,
             baca.system(measure=35, y_offset=20, distances=(15, 20)),
             baca.system(measure=42, y_offset=150, distances=(15, 20)),
-            number=3,
         ),
         baca.page(
+            4,
             baca.system(measure=51, y_offset=10, distances=(15, [20, 20, 25], 20)),
             baca.system(measure=59, y_offset=150, distances=(15, 20)),
-            number=4,
         ),
         baca.page(
+            5,
             baca.system(measure=68, y_offset=20, distances=(15, [20, 20, 25], 20)),
             baca.system(measure=75, y_offset=150, distances=(15, 20)),
-            number=5,
         ),
         baca.page(
+            6,
             baca.system(measure=83, y_offset=40, distances=(15, 20)),
             baca.system(measure=90, y_offset=170, distances=(15, 20)),
-            number=6,
         ),
         baca.page(
+            7,
             baca.system(
                 measure=98, y_offset=0, distances=(15, 25, (22.5, 25), (22.5, 20))
             ),
             baca.system(measure=104, y_offset=160, distances=(15, 20)),
-            number=7,
         ),
         baca.page(
+            8,
             baca.system(
                 measure=112, y_offset=20, distances=(15, 25, (22.5, 25), (22.5, 20))
             ),
             baca.system(measure=120, y_offset=150, distances=(15, 20)),
-            number=8,
         ),
         baca.page(
+            9,
             baca.system(measure=131, y_offset=20, distances=(15, 20)),
             baca.system(measure=141, y_offset=150, distances=(15, 20)),
-            number=9,
         ),
         baca.page(
+            10,
             baca.system(measure=147, y_offset=20, distances=(15, 20)),
             baca.system(measure=155, y_offset=150, distances=(15, 20)),
-            number=10,
         ),
         baca.page(
+            11,
             baca.system(measure=160, y_offset=20, distances=(15, 20)),
             baca.system(measure=166, y_offset=150, distances=(15, 20)),
-            number=11,
         ),
         baca.page(
+            12,
             baca.system(measure=172, y_offset=20, distances=(15, 20)),
             baca.system(measure=182, y_offset=150, distances=(15, 20)),
-            number=12,
         ),
         baca.page(
+            13,
             baca.system(measure=191, y_offset=20, distances=(15, 20)),
             baca.system(measure=198, y_offset=150, distances=(15, 20)),
-            number=13,
         ),
         baca.page(
+            14,
             baca.system(
                 measure=204,
                 y_offset=7.5,
@@ -82,27 +83,26 @@ spacing = baca.SpacingSpecifier(
                 y_offset=150,
                 distances=(15, 25, (20, 25), (20, 25), (20, 25)),
             ),
-            number=14,
         ),
         baca.page(
+            15,
             baca.system(measure=219, y_offset=40, distances=(15, 20)),
             baca.system(measure=225, y_offset=170, distances=(15, 20)),
-            number=15,
         ),
         baca.page(
+            16,
             baca.system(measure=231, y_offset=15, distances=(15, 20)),
             baca.system(
                 measure=239,
                 y_offset=120,
                 distances=(15, 25, (20, 25), (20, 25), (20, 25)),
             ),
-            number=16,
         ),
         baca.page(
+            17,
             baca.system(measure=247, y_offset=10, distances=(15, 20)),
             baca.system(measure=250, y_offset=110, distances=(15, 20)),
             baca.system(measure=254, y_offset=200, distances=(15, 20)),
-            number=17,
         ),
     ),
     fallback_duration=(1, 12),
--- ikribu/segments/13/layout.py
+++ ikribu/segments/13/layout.py
@@ -3,10 +3,12 @@ import baca
 spacing = baca.SpacingSpecifier(
     breaks=baca.breaks(
         baca.page(
+            1,
             baca.system(measure=1, y_offset=20, distances=(15, 15, 20, 22.5)),
             baca.system(measure=11, y_offset=140, distances=(15, 15, 20, 22.5)),
         ),
         baca.page(
+            2,
             baca.system(measure=20, y_offset=20, distances=(15, 15, 20, 22.5)),
             baca.system(measure=29, y_offset=140, distances=(15, 15, 20, 20)),
         ),
