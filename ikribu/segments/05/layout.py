666004d91b59992788435e9e8c468c0fc9f70c43
Trevor Bača <trevor.baca@gmail.com>
2021-08-24 15:03:34 -0400

    Cleaned up layout files.

--- ikribu/segments/05/layout.py
+++ ikribu/segments/05/layout.py
@@ -1,11 +1,9 @@
 import baca
 
-spacing = baca.SpacingSpecifier(
-    breaks=baca.breaks(
-        baca.page(
-            1,
-            baca.system(measure=1, y_offset=20, distances=(15, 15, 15, 20, 22.5)),
-        ),
+spacing = baca.make_layout(
+    baca.page(
+        1,
+        baca.system(measure=1, y_offset=20, distances=(15, 15, 15, 20, 22.5)),
     ),
     fallback_duration=(1, 16),
 )
--- ikribu/segments/14/layout.py
+++ ikribu/segments/14/layout.py
@@ -1,16 +1,14 @@
 import baca
 
-spacing = baca.SpacingSpecifier(
-    breaks=baca.breaks(
-        baca.page(
-            1,
-            baca.system(measure=1, y_offset=20, distances=(15, 20)),
-            baca.system(measure=14, y_offset=140, distances=(15, 20)),
-        ),
-        baca.page(
-            2,
-            baca.system(measure=24, y_offset=20, distances=(15, 20)),
-        ),
+spacing = baca.make_layout(
+    baca.page(
+        1,
+        baca.system(measure=1, y_offset=20, distances=(15, 20)),
+        baca.system(measure=14, y_offset=140, distances=(15, 20)),
+    ),
+    baca.page(
+        2,
+        baca.system(measure=24, y_offset=20, distances=(15, 20)),
     ),
     fallback_duration=(1, 12),
     overrides=(baca.space([7, 10], (1, 16)),),
