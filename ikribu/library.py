01ab659f2a24ffb3fc2439c4e6cf2bb938865095
Trevor Bača <trevor.baca@gmail.com>
2021-09-15 14:45:13 +0200

    Cleaned up empty score construction.

--- ikribu/library.py
+++ ikribu/library.py
@@ -321,7 +321,7 @@ class ScoreTemplate(baca.ScoreTemplate):
         site = "ikribu.ScoreTemplate.__call__()"
         tag = abjad.Tag(site)
         # GLOBAL CONTEXT
-        global_context = self._make_global_context()
+        global_context = baca.templates.make_global_context()
 
         # BASS CLARINET
         bass_clarinet_music_voice = abjad.Voice(
@@ -447,9 +447,9 @@ class ScoreTemplate(baca.ScoreTemplate):
 
         # SCORE
         score = abjad.Score([global_context, music_context], name="Score", tag=tag)
-        self._assert_lilypond_identifiers(score)
-        self._assert_unique_context_names(score)
-        # self._assert_matching_custom_context_names(score)
+        baca.templates.assert_lilypond_identifiers(score)
+        baca.templates.assert_unique_context_names(score)
+        # baca.templates.assert_matching_custom_context_names(score)
         return score
 
     ### PUBLIC PROPERTIES ##
