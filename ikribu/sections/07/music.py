0a517dd461a0af933ec05ac45c69a009b84a23a8
Trevor Bača <trevor.baca@gmail.com>
2022-08-16 08:03:12 -0400

    Bumped LilyPond to 2.23.11.

--- ikribu/sections/07/music.py
+++ ikribu/sections/07/music.py
@@ -164,7 +164,7 @@ def main():
     VN(accumulator.voice("Violin.Music"))
     VA(accumulator.voice("Viola.Music"))
     VC(accumulator.voice("Cello.Music"))
-    previous_persist = baca.previous_metadata(__file__, file_name="__persist__")
+    previous_persist = baca.previous_persist(__file__)
     baca.reapply(accumulator, accumulator.manifests(), previous_persist, voice_names)
     cache = baca.interpret.cache_leaves(
         score,
--- ikribu/sections/08/music.py
+++ ikribu/sections/08/music.py
@@ -157,7 +157,7 @@ def main():
     BCL(accumulator.voice("BassClarinet.Music"))
     MOST(score)
     VC(accumulator.voice("Cello.Music"))
-    previous_persist = baca.previous_metadata(__file__, file_name="__persist__")
+    previous_persist = baca.previous_persist(__file__)
     baca.reapply(accumulator, accumulator.manifests(), previous_persist, voice_names)
     cache = baca.interpret.cache_leaves(
         score,
