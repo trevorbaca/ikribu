76184b2e1598b2a4bfa83358704cf7346f7fd67b
Trevor Bača <trevor.baca@gmail.com>
2022-08-19 11:02:27 -0400

    Cleaned up music.py files.

--- ikribu/sections/01/music.py
+++ ikribu/sections/01/music.py
@@ -24,7 +24,7 @@ accumulator = baca.CommandAccumulator(
     short_instrument_names=library.short_instrument_names,
     metronome_marks=library.metronome_marks,
     time_signatures=time_signatures,
-    voice_abbreviations=library.voice_abbreviations,
+    _voice_abbreviations=library.voice_abbreviations,
     voice_names=voice_names,
 )
 
@@ -193,7 +193,7 @@ def main():
     cache = baca.interpret.cache_leaves(
         score,
         len(accumulator.time_signatures),
-        accumulator.voice_abbreviations,
+        library.voice_abbreviations,
     )
     bcl(cache["bcl"])
     vn_rh(cache["vn_rh"])
