a2312c62bb45889b27d200aafb23e2f6ab36f9e4
Trevor Bača <trevor.baca@gmail.com>
2022-09-26 08:27:53 -0400

    Cleaned up timing.

--- ikribu/sections/02/music.py
+++ ikribu/sections/02/music.py
@@ -110,6 +110,7 @@ def va(m):
         )
 
 
+@baca.build.timed
 def make_score(first_measure_number, previous_persistent_indicators):
     score, accumulator = make_empty_score()
     baca.section.set_up_score(
@@ -148,9 +149,11 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
+    timing = baca.build.Timing()
     score, accumulator = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
+        timing,
     )
     metadata, persist, timing = baca.build.postprocess_score(
         score,
