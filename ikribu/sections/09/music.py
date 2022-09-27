6b062bbf205365ca9244c06eff4709a656054e8b
Trevor Bača <trevor.baca@gmail.com>
2022-09-27 10:25:42 -0400

    Cleaned up metadata.

--- ikribu/sections/09/music.py
+++ ikribu/sections/09/music.py
@@ -194,8 +194,8 @@ def main():
     )
     baca.build.persist_lilypond_file(
         lilypond_file,
-        environment.metadata,
-        environment.persist,
+        metadata,
+        persist,
         timing,
         environment.arguments,
     )
