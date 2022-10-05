20e445d3385403d00db601e5541b242818145c98
Trevor Bača <trevor.baca@gmail.com>
2022-10-05 10:15:55 -0400

    Cleaned up tags.

--- ikribu/sections/09/music.py
+++ ikribu/sections/09/music.py
@@ -167,10 +167,6 @@ def main():
     metadata = baca.section.postprocess_score(
         score,
         **baca.section.section_defaults(),
-        activate=[
-            baca.tags.LOCAL_MEASURE_NUMBER,
-            baca.tags.STAGE_NUMBER,
-        ],
         always_make_global_rests=True,
         empty_fermata_measures=True,
         environment=environment,
@@ -178,6 +174,12 @@ def main():
         manifests=library.manifests,
         part_manifest=library.part_manifest(),
         transpose_score=True,
+        tags=baca.tags.Tags(
+            activate=[
+                baca.tags.LOCAL_MEASURE_NUMBER,
+                baca.tags.STAGE_NUMBER,
+            ],
+        ),
     )
     lilypond_file = baca.lilypond.file(
         score,
