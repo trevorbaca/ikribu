b4da6d873b3ea22ddda15a4a537653f12b59b376
Trevor Bača <trevor.baca@gmail.com>
2022-10-06 11:37:10 -0400

    Cleaned up music.py files.

--- ikribu/sections/13/music.py
+++ ikribu/sections/13/music.py
@@ -196,8 +196,8 @@ def make_score(first_measure_number, previous_persistent_indicators):
     VC(voices("Cello.Music"), measures)
     baca.section.reapply(
         voices,
-        library.manifests,
         previous_persistent_indicators,
+        manifests=library.manifests,
     )
     cache = baca.section.cache_leaves(
         score,
