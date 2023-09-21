9b8a292ae753575a6c662cc5de33e2a93f68ead5
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:45 -0400

    Cleaned up music.py files.

--- ikribu/sections/15/music.py
+++ ikribu/sections/15/music.py
@@ -235,5 +235,20 @@ def main():
     )
 
 
+def make_layout():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(
+                measure=1,
+                y_offset=20,
+                distances=(15, 20, 22.5, 25, 22.5, 25, 22.5),
+            ),
+        ),
+        spacing=(1, 16),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
